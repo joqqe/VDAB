4e16f9f02828d8c8bf5a85c7fe27119bb7ba3b6f
Jochen Schoonjans <Jochen.Schoonjans@realdolmen.com>
2021-05-07 16:35:13 +0200

    add oef5 dag 2

---
+++ Dag2/Spotify - oef5/main.js
@@ -0,0 +1,37 @@
+function rowSelect(e) {
+    var rows = document.querySelectorAll("table tbody tr.active");
+    for (const row of rows) {
+        row.classList.remove("active");
+    }
+
+    e.currentTarget.classList.add("active");
+}
+
+var songs = ["Title1", "Title2"];
+
+function renderTable() {
+    // Remove all rows
+    var rows = document.querySelectorAll("table tbody tr");
+    for (const row of rows) {
+        row.remove();
+    }
+
+    // Add new rows
+    var tbody = document.querySelector("table tbody");
+    for (const song of songs) {
+        var tr = document.createElement("tr");
+        var td = document.createElement("td");
+        var text = document.createTextNode(song);
+
+        td.appendChild(text);
+        tr.appendChild(td);
+        tbody.appendChild(tr);
+    }
+}
+
+renderTable();
+
+var rows = document.querySelectorAll("table tbody tr");
+for (const row of rows) {
+    row.addEventListener("click", rowSelect);
+}
