461a48f5910a123e8b15d6bbc9ebbc4c64d3b9c2
Jochen Schoonjans <Jochen.Schoonjans@realdolmen.com>
2021-05-09 20:55:57 +0200

    add oef closure

---
+++ Dag3/Oef - objecten/Closure 1/main.js
@@ -0,0 +1,32 @@
+function Vehicle(power) {
+    return {
+        power: power,
+        start: function() {
+            return this.power + " doet vroem vroem...";
+        }
+    };
+}
+
+function Car(power, make, model, year) {
+    var fuel = "?";
+
+    return {
+        ...Vehicle.call(this, power),
+        make: make,
+        model: model,
+        year: year,
+        getFullName: function() {
+            fuel = "petrol";
+            return this.model + " " + this.power + " " + fuel;
+        }
+    };
+}
+
+var audiA1 = Car('100pk', 'Audi', 'A1', 2021);
+console.log(audiA1.start()); // "100pk doet vroem vroem..."
+console.log(audiA1.getFullName()); // "A1 100pk petrol"
+
+console.log("Car: ", audiA1 instanceof Car); // false
+console.log("Vehicle: ", audiA1 instanceof Vehicle); // false
+
+console.log(audiA1.constructor.name); // Object
